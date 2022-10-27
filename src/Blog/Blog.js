b3e957958983b83ecd3b71bf33b9d8b4fbd3d9fe
mdshahjhan <mdshahjha478877@gmail.com>
2022-10-27 10:45:00 +0600

    The privat

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -9,10 +9,18 @@ const Blog = () => {
                 <p class="mb-2 text-gray-500 dark:text-gray-400">Cross-origin resource sharing is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served. </p>
 
             </div>
-                <span>Why are you using ?</span>
+                <span>Why are you using ? What other options do you have to implement authentication?</span>
 
             <div class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
                 <p class="mb-2 text-gray-500 dark:text-gray-400">Firebase is a mobile platform that helps you quickly develop high-quality apps, grow your user base, and earn more money. Firebase is made up of complementary features that you can mix-and-match to fit your needs, with Google Analytics for Firebase at the </p>
+                <p>Usually, authentication by a server entails the use of a user name and password. Other ways to authenticate can be through cards, retina scans, voice recognition, and fingerprints.</p>
+
+            </div>
+          
+                <span> How does the private route work?</span>
+
+            <div class="p-5 font-light border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                <p class="mb-2 text-gray-500 dark:text-gray-400">The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated </p>
 
             </div>
           
